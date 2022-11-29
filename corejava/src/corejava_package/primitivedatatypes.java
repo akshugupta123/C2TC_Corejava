969c775b4e7d995b9a451add16d2370d06f8fe6e
akshugupta123 <akshathagupta888@gmail.com>
2022-11-29 19:54:28 +0530

    primitivedatatype

---
+++ corejava/src/corejava_package/primitivedatatypes.java
@@ -0,0 +1,32 @@
+package corejava_package;
+
+public class primitivedatatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 short temperature;
+	    temperature = -200;
+	    int range2 = -4250000;
+	    long range3 = -42332200000L;
+	    double number = -42.3;
+	    float number1 = -42.3f;
+	    boolean flag = true;
+        byte range1;
+        range1 = 124;
+        char letter = '\u0051';
+        char letter1 = '9';
+        System.out.println(letter1);   	
+        char letter2 = 65;
+        System.out.println(letter2); 
+        System.out.println(letter);
+          System.out.println(number1);
+          System.out.println(range1); 
+          System.out.println(flag);
+          System.out.println(temperature);
+          System.out.println(range2);
+          System.out.println(range3); 
+          System.out.println(number); 
+          
+	}
+
+}

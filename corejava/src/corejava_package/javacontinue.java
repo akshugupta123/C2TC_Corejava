3509a924fa6a4a2f7824ca9bc7f4bb6ae3807bd4
akshugupta123 <akshathagupta888@gmail.com>
2022-11-29 21:40:12 +0530

    javacommit

---
+++ corejava/src/corejava_package/javacontinue.java
@@ -0,0 +1,16 @@
+package corejava_package;
+
+public class javacontinue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 10; i++) {
+			  if (i == 4) {
+			    continue;
+			  }
+			  System.out.println(i);
+			}
+
+	}
+
+}
